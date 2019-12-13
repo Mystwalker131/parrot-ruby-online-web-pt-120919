4bc63b0016bee46b24154fdf9649eadd4c2565a9
Benito Galvez <mystwalker131@gmail.com>
2019-12-13 03:53:03 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+<<<<<<< HEAD
 
 
 
@@ -8,3 +9,11 @@ def parrot(phrase="Squawk!")
   return phrase
 end
 
+=======
+def parrot(string="Squawk!")
+  puts(string="Lady Bird!")
+  return "Squawk!"
+end
+
+parrot"Squawk!"
+>>>>>>> 9ff296380fab4e081015dab155d78117e519bde7
